7e747c7c70bc37fa64d9f799980f97d577cd16b8
richellemaria <rishdsouza@gmail.com>
2024-02-01 20:09:23 +0530

    auto push

---
+++ vars/nodejs.groovy
@@ -0,0 +1,29 @@
+def Lintcheck(){
+   sh '''
+        sh "echo installing jslint"
+        sh "npm i jslint"
+        sh "node_modules/jslint/bin/jslint.js server.js || true"
+    '''            
+}                
+
+def call(){
+    pipeline{
+    agent {
+        label 'WS'
+    }
+    stages{
+        stage('Lint Check'){
+            steps{
+                script{
+                 nodejs.Lintcheck()
+                }
+            }
+        }
+        stage('Code Complie'){
+            steps{
+                sh "npm install"
+            }
+        }
+    }
+    } 
+} 
