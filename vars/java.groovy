0d63d33f355c76b48e506b73d884cd8d7277ce59
richellemaria <rishdsouza@gmail.com>
2024-02-01 20:27:12 +0530

    auto push

---
+++ vars/java.groovy
@@ -0,0 +1,29 @@
+def Lintcheck(){
+   sh '''
+        echo Performing lintcheck for  ${COMPONENT}
+        mvn checkstyle:check || true
+        echo lint checks completed for ${COMPONENT}
+    '''            
+}                
+
+def call(COMPONENT){
+    pipeline{
+    agent {
+        label 'WS'
+    }
+    stages{
+        stage('Lint Check'){
+            steps{
+                script{
+                  Lintcheck()
+                }
+            }
+        }
+        stage('Code Complie'){
+            steps{
+                sh "npm install"
+            }
+        }
+    }
+    } 
+} 
