8db75c7912de0fb49c14bd09decdf37e091663b7
richellemaria <rishdsouza@gmail.com>
2024-02-02 11:18:04 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,7 +4,15 @@ def Lintcheck(){
         npm i jslint
         node_modules/jslint/bin/jslint.js server.js || true
     '''            
-}                
+}  
+
+def Sonarcheck(){
+   sh '''
+        echo Sonar check Inprogress
+        sonar-scanner -Dsonar.source=. -Dsonar.login=971b90e060ec03306b76af963845170e8e5b5687 -Dsonar.host.url=http://172.31.6.159:9000 -Dsonar.projectKey=${COMPONENT}
+        echo Sonar check Completed
+    '''            
+} 
 
 def call(COMPONENT){
     pipeline{
@@ -22,6 +30,18 @@ def call(COMPONENT){
         stage('Code Complie'){
             steps{
                 sh "npm install"
+            }
+        }
+        stage('Sonar check'){
+            steps{
+                script{
+                  Sonarcheck()
+                }
+            }
+        }
+        stage('Testing'){
+            steps{
+                sh "echo testing inprogress"
             }
         }
     }
