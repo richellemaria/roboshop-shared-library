84eb226a3f3dc9ae3500991a5c1da71f68606f94
richellemaria <rishdsouza@gmail.com>
2024-02-06 22:23:24 +0530

    auto push

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,41 +1,47 @@
-def Lintcheck(){
-   sh '''
-       echo Performing lintcheck for  ${COMPONENT}
-       #pip3 install pylint
-       echo lint checks completed for ${COMPONENT}
-    '''            
-}                
+def call(){
+ node{
+   common.Lintcheck()
+ }
+}
 
-def call(COMPONENT){
-    pipeline{
-    agent {
-        label 'WS'
-    }
-    enivronment{
-        SONARCRED=credentials('SONARCRED')
-        SONARURL="172.31.6.159"
-    }
-    stages{
-        stage('Lint Check'){
-            steps{
-                script{
-                  Lintcheck()
-                }
-            }
-        }
-        stage('Sonar check'){
-            steps{
-                script{
-                  env.ARGS="-Dsonar.source=."  
-                  common.Sonarcheck()
-                }
-            }
-        }
-        stage('Testing'){
-            steps{
-                sh "echo testing inprogress"
-            }
-        }
-    }
-    } 
-} 
+// def Lintcheck(){
+//    sh '''
+//        echo Performing lintcheck for  ${COMPONENT}
+//        #pip3 install pylint
+//        echo lint checks completed for ${COMPONENT}
+//     '''            
+// }                
+
+// def call(COMPONENT){
+//     pipeline{
+//     agent {
+//         label 'WS'
+//     }
+//     enivronment{
+//         SONARCRED=credentials('SONARCRED')
+//         SONARURL="172.31.6.159"
+//     }
+//     stages{
+//         stage('Lint Check'){
+//             steps{
+//                 script{
+//                   Lintcheck()
+//                 }
+//             }
+//         }
+//         stage('Sonar check'){
+//             steps{
+//                 script{
+//                   env.ARGS="-Dsonar.source=."  
+//                   common.Sonarcheck()
+//                 }
+//             }
+//         }
+//         stage('Testing'){
+//             steps{
+//                 sh "echo testing inprogress"
+//             }
+//         }
+//     }
+//     } 
+// } 
