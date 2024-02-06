d0dee952b88111a0eafed2989f51f1a0d0fe6de2
richellemaria <rishdsouza@gmail.com>
2024-02-06 22:16:37 +0530

    auto push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,36 @@
+def Lintcheck(){
+        stage('Lint Check'){
+                if(env.APP_TYPE == "nodejs"){
+                        sh '''
+                                echo installing jslint ${COMPONENT}
+                                npm i jslint
+                                node_modules/jslint/bin/jslint.js server.js || true
+                        ''' 
+                } 
+                else if(env.APP_TYPE == "python") {
+                        sh '''
+                                pip3 install pylint
+                                pip *.py
+                                echo lint checks started for ${COMPONENT} using pylint
+                                echo lint checks completed for ${COMPONENT}
+                        '''
+                }
+                else if(env.APP_TYPE == "java") {
+                        sh '''
+                                mvn checkstyle:check
+                                echo lint checks started for ${COMPONENT} 
+                                echo lint checks completed for ${COMPONENT}
+                        '''
+                }
+                else{
+                 sh '''       
+                   echo lint checks started for ${COMPONENT} using angularjs 
+                   echo lint checks completed for ${COMPONENT} 
+                  '''     
+                }
+        }          
+}
+
 def Sonarcheck(){
         sh "echo Sonar check Inprogress"
         sh "sonar-scanner -Dsonar.login=admin -Dsonar.password=password -Dsonar.host.url=http://172.31.6.159:9000 -Dsonar.projectKey=${COMPONENT} ${ARGS}"
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,76 +6,102 @@ def Lintcheck(){
     '''            
 }  
 
+def call()
+{
+    node {
+        common.LintCheck()
+    }
+}
 
-def call(COMPONENT){
-    pipeline{
-    agent {
-        label 'WS'
-    }
-    enivronment{
-        SONARCRED=credentials('SONARCRED')
-        NEXUS=credentials('NEXUS')
-        SONARURL="172.31.6.159"
-    }
-    stages{
-        stage('Lint Check'){
-            steps{
-                script{
-                  Lintcheck()
-                }
-            }
-        }
-        stage('Code Complie'){
-            steps{
-                sh "npm install"
-            }
-        }
-        stage('Sonar check'){
-            steps{
-                script{
-                  env.ARGS="-Dsonar.source=."  
-                  common.Sonarcheck()
-                }
-            }
-        }
-        stage('Testing'){
-            parallel{
-                stage('Unit Testing'){
-                    steps{
-                        sh "echo unit testing started"
-                        sh "echo unit testinbg completed" 
-                    }
-                }
-                stage('Intergration Testing'){
-                    steps{
-                        sh "echo Intergration testing started"
-                        sh "echo Intergration testinbg completed" 
-                    }
-                }
-                stage('Functional Testing'){
-                    steps{
-                        sh "echo Functional testing started"
-                        sh "echo Functional testinbg completed" 
-                    }
-                }
-            }
-        }
-        stage('Perpare the artifacts'){
-            when{ expression { env.TAG_NAME != null } }
-            steps{
-                sh "echo Prepare artifacts ${COMPONENT}"
-                sh "npm install"
-                sh "zip ${COMPONENT}.zip node_modules server.js"
-            }
-        }
-        stage('upload Artifacts'){
-            when{ expression { env.TAG_NAME != null } }
-            steps{
-                sh "echo Uploading artifacts to nexus"
-                sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload -file ${COMPOMENT}.${TAG_NAME}.zip http://localhost:8081/repository/${COMPOMENT}/${COMPOMENT}.${TAG_NAME}.zip"
-                sh "echo Uploading artifacts to nexus is completed"
-            }
-        }
-    }
-    } 
-} 
+// def call(COMPONENT){
+//     pipeline{
+//     agent {
+//         label 'WS'
+//     }
+//     enivronment{
+//         SONARCRED=credentials('SONARCRED')
+//         NEXUS=credentials('NEXUS')
+//         SONARURL="172.31.6.159"
+//         NEXUSURL=""
+//     }
+//     stages{
+//         stage('Lint Check'){
+//             steps{
+//                 script{
+//                   Lintcheck()
+//                 }
+//             }
+//         }
+//         stage('Code Complie'){
+//             steps{
+//                 sh "npm install"
+//             }
+//         }
+//         stage('Sonar check'){
+//             steps{
+//                 script{
+//                   env.ARGS="-Dsonar.source=."  
+//                   common.Sonarcheck()
+//                 }
+//             }
+//         }
+//         stage('Testing'){
+//             parallel{
+//                 stage('Unit Testing'){
+//                     steps{
+//                         sh "echo unit testing started"
+//                         sh "echo unit testinbg completed" 
+//                     }
+//                 }
+//                 stage('Intergration Testing'){
+//                     steps{
+//                         sh "echo Intergration testing started"
+//                         sh "echo Intergration testinbg completed" 
+//                     }
+//                 }
+//                 stage('Functional Testing'){
+//                     steps{
+//                         sh "echo Functional testing started"
+//                         sh "echo Functional testinbg completed" 
+//                     }
+//                 }
+//             }
+//         }
+//         stage('Validate the artifacts version '){
+//             when{ 
+//                 expression { env.TAG_NAME != null } 
+//                 }
+//             steps{
+//                 env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://${NEXUSURL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true')
+//                 print UPLOAD_STATUS
+//             }
+//         }
+//         stage('Perpare the artifacts'){
+//             when{ 
+//                 expression { env.TAG_NAME != null } 
+//                 expression { env.UPLOAD_STATUS == "" }
+//                 }
+//             steps{
+//                 sh '''
+//                   echo Prepare artifacts ${COMPONENT}
+//                   npm install
+//                   zip ${COMPONENT}.${TAG_NAME}.zip node_modules server.js
+//                 '''
+//             }
+//         }
+//         stage('upload Artifacts'){
+//             when{ 
+//                 expression { env.TAG_NAME != null } 
+//                 expression { env.UPLOAD_STATUS == "" }
+//                }
+//             steps{
+//                 sh '''
+//                   echo Uploading artifacts to nexus
+//                   curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload -file ${COMPOMENT}.${TAG_NAME}.zip http://localhost:8081/repository/${COMPOMENT}/${COMPOMENT}.${TAG_NAME}.zip
+//                   echo Uploading artifacts to nexus is completed
+//                 '''
+//             }
+//         }
+//     }
+//     } 
+// } 
